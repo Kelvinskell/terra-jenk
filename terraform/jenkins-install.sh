ac1819fec1fb708e5308ad8629b0fc658a9bc0e5
Kelvinskell <kelvinskelll@gmail.com>
2024-02-12 10:35:57 +0100

    Add user data

---
+++ terraform/jenkins-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt-get install openjdk-11-jdk -y
+sudo apt install jenkins -y
