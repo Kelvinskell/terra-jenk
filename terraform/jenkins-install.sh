ffb185d6eafdffeb1c3c3eb10f50d4795d773c26
Kelvinskell <kelvinskelll@gmail.com>
2024-02-12 10:36:20 +0100

    update user data

--- terraform/jenkins-install.sh
+++ terraform/jenkins-install.sh
@@ -8,5 +8,5 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
-sudo apt-get install openjdk-11-jdk -y
+sudo apt-get install openjdk-17-jdk -y
 sudo apt install jenkins -y
